601e86563f901bf31b35778907c782e8f5989c8a
Jim Kennedy <jgkennedy@rentworx.com>
2020-01-01 13:20:12 -0500

    Added comment

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -8,6 +8,8 @@ import java.util.Optional;
 /**
  * Created by jt on 6/13/17.
  */
+
+// don't need annotation here
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
     Optional<Category> findByDescription(String description);
